907ecb5f723e7190115962bb9b0e8cc9e35a39d7
Burmikk <116118020+Burmikk@users.noreply.github.com>
2022-12-03 12:19:32 +0000

    Deploying to gh-pages from @ Burmikk/ITeam-project@9c78b064755820d0454719a2d953d64d33a984a6 🚀

---
+++ index.a95d5921.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-a-modal-open]"),closeModalBtn:document.querySelector("[data-a-modal-close]"),modal:document.querySelector("[data-a-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a95d5921.js.map
